f575e9b9672b7777de195012df65293b607f9df6
Deep Shankar Pandey <pandeydeep9g@gmail.com>
2018-09-20 10:38:58 +0545

    test cv train, fixed a big bug(Y reading was wrong

--- Main.py
+++ Main.py
@@ -10,17 +10,34 @@ import Sigmoid
 import InitializeParameters
 import CostFunction
 
-filename = "semeion.data.txt"
+filename = "semeion.data"
 savefile = "tempFile.txt"
 
 filedata = open(filename,"r")
 
 array = (np.loadtxt(filedata)).astype(np.int64)
 X = array[:,:-10]
-Y = array[:,:10]
+Y = array[:,-10:]
+
+print(Y[0:1500:50,:])
 
 #add x0 = 1
 X = np.hstack((np.ones((X.shape[0],1)),X))
+
+#m no of data
+m = X.shape[0]
+train_size = int(0.6*m)
+cv_size = int(0.8*m)
+
+X_train = X[:train_size,:]
+X_cv    = X[train_size:cv_size,:]
+X_test  = X[cv_size:,:]
+
+Y_train = Y[:train_size,:]
+Y_cv    = Y[train_size:cv_size,:]
+Y_test  = Y[cv_size:,:]
+
+#print("x" + str(X_train.shape) + " a " + str(X_cv.shape) + " b " + str(X_test.shape))
 #print(len(Y[:,1]))
 #print(len(X[1,:]))
 
@@ -38,21 +55,32 @@ Theta = InitializeParameters.initParams(X.shape,Y.shape)
 #print(b.shape)
 
 
-print(X.shape)
-z = np.dot(X,Theta)
+#print(X.shape)
+#Train the model
+z = np.dot(X_train,Theta)
 h = Sigmoid.sigmoid(z)
 
-#no of data
-m = X.shape[0]
-J = CostFunction.cost(h,Y,m)
-print(J)
+
+J = CostFunction.cost(h,Y_train,m)
+#print(J)
 
 learning_rate = 0.01
-for i in range(100000):
-    dTheta =  1/m *np.dot( (h-Y).T,X ).T
+for i in range(1):
+    dTheta =  1/m *np.dot( (h-Y_train).T,X_train ).T
     #print(dTheta.shape)
     Theta = Theta - learning_rate * dTheta
-    z = np.dot(X,Theta)
+    z = np.dot(X_train,Theta)
     h = Sigmoid.sigmoid(z)
-    J = CostFunction.cost(h,Y,m)
+    J = CostFunction.cost(h,Y_train,m)
     print(J)
+
+
+z = np.dot(X_test,Theta)
+h = Sigmoid.sigmoid(z)
+theCost  = CostFunction.cost(h,Y_test,m)
+pred = np.zeros(h.shape)
+pred = np.where(h>0.5,1,0)
+
+VisualizeData.visualize(X_test[50,1:],1)
+print(Y[0:1500:100,:])
+print(pred[100,:])
--- VisualizeData.py
+++ VisualizeData.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 
-def visualize(arr,noOfData):
+def visualize(arr,noOfData=1):
     dispImg = arr.reshape(16,16,noOfData) *256
     finalImg = dispImg[:,:,0]
     for i in range(1,noOfData):
@@ -11,7 +11,7 @@ def visualize(arr,noOfData):
 
     print(finalImg.shape)
     finalImg2 = finalImg[:,0:160]
-    for i in range(1,10):
+    for i in range(1,int(np.sqrt(noOfData))):
         finalImg2 = np.vstack((finalImg2,finalImg[:,160*i:160*(i+1)]))
     print(finalImg2.shape)
     cv2.imwrite('disp_img.PNG', finalImg2)
